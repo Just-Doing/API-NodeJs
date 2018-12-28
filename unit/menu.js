c7cca0ded4ecc188a36ea3095d72f6889d586efd
NicolYang <969502899@qq.com>
2018-12-28 23:59:49 +0800

    unit test

--- unit/menu.js
+++ unit/menu.js
@@ -6,29 +6,29 @@ require( "should" );
 
 const {expect} = chai;
 
-it( "api:users/get ==> should return users", ( done ) => {
-    http.get( "http://localhost:3000/system/users/get", ( res ) => {
+it( "api:menu/get ==> should return menus", ( done ) => {
+    http.get( "http://localhost:3000/system/menu/get", ( res ) => {
         res.setEncoding( "utf8" );
         res.on( "data", ( text ) => {
             assert.equal( res.statusCode, 200 );
             const result = JSON.parse( text );
             result.should.have.property( "total" );
-            result.should.have.property( "users" );
+            result.should.have.property( "menus" );
             done();
         } );
     } );
 } );
 
-it( "api:users/put ==> should return a object", ( ) => {
-    axios.post( "http://localhost:3000/system/users/put", {} ).then( ( res ) => {
+it( "api:menu/put ==> should return a object", ( ) => {
+    axios.post( "http://localhost:3000/system/menu/put", {} ).then( ( res ) => {
         assert.equal( res.statusCode, 200 );
         expect( res ).to.be.an( "object" );
     } );
 } );
 
 
-it( "api:users/setRole ==> should return a object", ( ) => {
-    axios.post( "http://localhost:3000/system/users/setRole", {} ).then( ( res ) => {
+it( "api:menu/setpower ==> should return a object", ( ) => {
+    axios.post( "http://localhost:3000/system/menu/setMenu", {} ).then( ( res ) => {
         assert.equal( res.statusCode, 200 );
         expect( res ).to.be.an( "object" );
     } );
